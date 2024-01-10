e654c3f76f1f94ac4ef845939806f295e19fc091
Abduljebar <Abduljebar49@gmail.com>
2024-01-10 18:31:08 +0300

    logo's for sidebar added

---
+++ src/assets/indexlogo.js
@@ -0,0 +1,7 @@
+import UndoLogo from "./logo/undo.svg";
+import RedoLogo from "./logo/redo.svg";
+import ZoomLogo from "./logo/zoom.svg";
+import ClearLogo from "./logo/clear.svg";
+import SideLogo from "./logo/side.svg";
+
+export { UndoLogo, RedoLogo, ZoomLogo, ClearLogo, SideLogo };
