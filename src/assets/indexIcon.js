491f486aece7fa647bc1fa71291416b71425fa92
Abduljebar <Abduljebar49@gmail.com>
2024-01-10 18:30:35 +0300

    icons for sidebar added to asset folder

---
+++ src/assets/indexIcon.js
@@ -0,0 +1,7 @@
+import uploadIcon from "./icons/upload.svg";
+import textIcon from "./icons/text.svg";
+import artIcon from "./icons/art.svg";
+import arrowIcon from "./icons/arrow.svg";
+import arrow1Icon from "./icons/arrow1.svg";
+
+export { uploadIcon, textIcon, artIcon, arrowIcon, arrow1Icon };
